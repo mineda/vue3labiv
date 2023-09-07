b130fdb85681320f43ce4e925e2b3ce573fa62eb
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-09-07 01:43:45 +0000

    CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios';
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+
+axios.defaults.baseURL="https://8080-mineda-springboot3labiv-hkgigshkty9.ws-us104.gitpod.io/"
 
 const app = createApp(App)
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,63 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>This is an about page, {{ nome }}</p>
+    <p>Nome: <input type="text" v-model="nome"/></p>
+    <p v-if="nome.length > 5">Texto muito longo!</p>
+    <p v-else>Texto com tamanho normal!</p>
+    <p>Nome: <input type="password" v-model="senha"/></p>
+    <button @click="salvarUsuario">Inserir</button>
+    <button @click="buscarUsuarios">Atualizar</button>
+    <p v-if="erro">{{ erro }}</p>
+    <table>
+      <thead>
+        <td>Id</td>
+        <td>Nome</td>
+      </thead>
+      <tbody>
+        <tr v-for="usuario in usuarios">
+          <td>{{ usuario.id }}</td>
+          <td>{{ usuario.nome }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+  import axios from 'axios';
+
+  const nome = ref("Teste");
+  const senha = ref("123");
+  const erro = ref();
+  const usuarios = ref();
+
+  async function buscarUsuarios() {
+    erro.value = "";
+    try {
+      usuarios.value = (await axios.get("usuario")).data;
+    }
+    catch(error) {
+      erro.value = (error as Error).message;
+    }
   }
-}
-</style>
+
+  async function salvarUsuario() {
+    erro.value = "";
+    try {
+      usuarios.value.push((await axios.post("usuario", 
+          {
+            nome: nome.value,
+            senha: senha.value
+          }
+        )).data);
+    }
+    catch(error) {
+      erro.value = (error as Error).message;
+    }
+  }
+
+  onMounted(() => {
+    buscarUsuarios();
+  });
+</script>
